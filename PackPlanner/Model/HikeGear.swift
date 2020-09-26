4825d9de3f64b6335ca8723039c7dd9059dcf960
Anand Kumar Sankaran <lists@anands.net>
2020-09-26 11:20:16 -0700

    Added notes

--- PackPlanner/Model/HikeGear.swift
+++ PackPlanner/Model/HikeGear.swift
@@ -14,6 +14,7 @@ class HikeGear : Object {
     @objc dynamic var consumable: Bool = false
     @objc dynamic var worn: Bool = false
     @objc dynamic var numberUnits: Int = 1
-    @objc dynamic var verified: Bool = false 
+    @objc dynamic var verified: Bool = false
+    @objc dynamic var notes:String = ""
     var parentHike = LinkingObjects(fromType: Hike.self, property: "hikeGears")
 }
